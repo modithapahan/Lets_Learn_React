55b90087adc635032d9fc6df2db0bbc2db375229
modithapahan <modithapahan0@gmail.com>
2022-09-27 09:59:06 +0530

    Learn about props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React is easy</h1>
+      <User name="Moditha0" age={22} email="modithapahan0@gmail.com" />
+      <User name="Moditha1" age={22} email="modithapahan1@gmail.com" />
+      <User name="Moditha2" age={22} email="modithapahan2@gmail.com" />
+    </div>
+  );
+}
+
+const User = (props) => {
+  return (
+    <div class="container mt-4 pt-3">
+      <h4>{props.name}</h4>
+      <h4>{props.age}</h4>
+      <h4>{props.email}</h4>
     </div>
   );
 }
