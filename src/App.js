dd010da8fc9cde59410d958eb618e77c82325e3c
modithapahan <modithapahan0@gmail.com>
2022-09-28 10:10:25 +0530

    Display data using lists

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import User from "./User";
 
 function App() {
-
-  const age = 42;
-  const isGreen = true;
-  let mango = 420;
-  let apple = 150;
-  const sum = mango + apple;
+  const users = [
+    { name: "moditha1", age: 22 },
+    { name: "moditha2", age: 23 },
+    { name: "moditha3", age: 23 },
+  ];
 
   return (
-    <div className="App">
-      <div>
-        {age >= 18 ? <h4>Over Age</h4> : <h4>Under Age</h4>}
-        <h1 style={{ color: isGreen ? "green" : "red" }}>This is a Boolean.</h1>
-      </div>
-
-      <div>
-        {sum >= 500 ? <h4>Price is too high</h4> : <h4>Price is normal</h4>}
-      </div>
+    <div className="container mt-3">
+      {users.map((user, key) => {
+        return <User name={user.name} age={user.age} />
+      })}
     </div>
   );
 }
